e0b432eced8634a90facbd46f8a2f0e91dfacdf1
Shailee Eliyahu <shailee.eliyahu@gmail.com>
2020-10-06 15:50:50 +0300

    small imrovement in code cleanliness

--- src/App/App.js
+++ src/App/App.js
@@ -3,6 +3,5 @@ import GameplayScreen from 'Screens/GameplayScreen/GameplayScreen';
 import { renderOnlyIfBrowserValid } from './AppUtils';
 
 export default function App() {
-  const contentAfterBrowserCheck = renderOnlyIfBrowserValid(<GameplayScreen />);
-  return contentAfterBrowserCheck;
+  return renderOnlyIfBrowserValid(<GameplayScreen />);
 }
--- src/App/AppUtils.js
+++ src/App/AppUtils.js
@@ -1,16 +1,20 @@
 import { errorMessagesTexts } from 'Texts/gameplayTexts';
+
 export function renderOnlyIfBrowserValid(contentToRender) {
+  if (isBrowserValid()) {
+    return contentToRender;
+  } else {
+    return errorMessagesTexts.invalidBrowser;
+  }
+}
+
+export function isBrowserValid() {
   // Internet Explorer 6-11
   const isIE = false || !!document.documentMode || navigator.userAgent.indexOf('MSIE') !== -1;
 
   // Edge 20-78 (not Chromium based)
   const isOldEdge = !isIE && !!window.StyleMedia;
 
-  const isBrowserValid = !(isIE || isOldEdge);
-
-  if (isBrowserValid) {
-    return contentToRender;
-  } else {
-    return errorMessagesTexts.invalidBrowser;
-  }
+  const isBrowserValid = !isIE && !isOldEdge;
+  return isBrowserValid;
 }
