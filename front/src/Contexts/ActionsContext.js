20fcac0ae40794bc7cc77c08e7a8c199d7171b5d
elShailee <shailee.eliyahu@gmail.com>
2021-03-07 10:07:02 +0200

    Small fix towards pr
    forgot to remove 1 commented line,
    ended up reformatting  the file, which is nice

--- front/src/Contexts/ActionsContext.js
+++ front/src/Contexts/ActionsContext.js
@@ -1,5 +1,4 @@
 import React, { Component, createContext } from 'react';
-//import activeActions from 'ExampleData/activeActionsExampleData'; //Switched to Server
 import actionsLibrary from 'ExampleData/actionsLibraryExampleData';
 import ActionsTabItem from 'Components/Actions/ActionsTabItem';
 import { fetchJSON } from 'Utils/restFuncs';
@@ -7,75 +6,75 @@ import { fetchJSON } from 'Utils/restFuncs';
 export const ActionsContext = createContext();
 
 class ActionsContextProvider extends Component {
-  state = { actions: {}, actionsOrder: {} };
-  actionsItemsTypes = { tabItem: 'tabItem' };
+	state = { actions: {}, actionsOrder: {} };
+	actionsItemsTypes = { tabItem: 'tabItem' };
 
-  async componentDidMount() {
-    const URL = 'http://localhost:8000/api/actions/';
-    const activeActions = await fetchJSON(URL);
-    this.setState({ actions: activeActions, actionsOrder: this.getActionsOrder(activeActions) });
-  }
+	async componentDidMount() {
+		const URL = 'http://localhost:8000/api/actions/';
+		const activeActions = await fetchJSON(URL);
+		this.setState({ actions: activeActions, actionsOrder: this.getActionsOrder(activeActions) });
+	}
 
-  getActionsOrder = activeActions => {
-    const actionsOrder = [];
-    for (const action in activeActions) {
-      actionsOrder.push(action);
-    }
-    return actionsOrder;
-  };
+	getActionsOrder = activeActions => {
+		const actionsOrder = [];
+		for (const action in activeActions) {
+			actionsOrder.push(action);
+		}
+		return actionsOrder;
+	};
 
-  applyActionsReorder = result => {
-    const { actionsOrder } = this.state;
-    const { source, destination, draggableId } = result;
+	applyActionsReorder = result => {
+		const { actionsOrder } = this.state;
+		const { source, destination, draggableId } = result;
 
-    if (!destination) return;
-    if (source.index === destination.index && source.draggableId === destination.draggableId) return;
+		if (!destination) return;
+		if (source.index === destination.index && source.draggableId === destination.draggableId) return;
 
-    let newActionsOrder = Array.from(actionsOrder);
-    newActionsOrder.splice(source.index, 1);
-    newActionsOrder.splice(destination.index, 0, draggableId);
-    this.setState({ actionsOrder: newActionsOrder });
-  };
+		let newActionsOrder = Array.from(actionsOrder);
+		newActionsOrder.splice(source.index, 1);
+		newActionsOrder.splice(destination.index, 0, draggableId);
+		this.setState({ actionsOrder: newActionsOrder });
+	};
 
-  renderActionTabItems = numOfItemsToRender => {
-    return this.renderActionItems(this.actionsItemsTypes.tabItem, numOfItemsToRender);
-  };
+	renderActionTabItems = numOfItemsToRender => {
+		return this.renderActionItems(this.actionsItemsTypes.tabItem, numOfItemsToRender);
+	};
 
-  renderActionItems = (renderType, numOfItemsToRender) => {
-    const { actionsOrder } = this.state;
-    if (!numOfItemsToRender) numOfItemsToRender = actionsOrder.length;
+	renderActionItems = (renderType, numOfItemsToRender) => {
+		const { actionsOrder } = this.state;
+		if (!numOfItemsToRender) numOfItemsToRender = actionsOrder.length;
 
-    const itemsToRenderIds = actionsOrder.slice(0, numOfItemsToRender);
-    const renderedActionItems = itemsToRenderIds.map((itemId, index) => {
-      const actionObject = this.generateActionObject(itemId);
-      return this.generateActionItemJSX(actionObject, index, renderType);
-    });
-    return renderedActionItems;
-  };
+		const itemsToRenderIds = actionsOrder.slice(0, numOfItemsToRender);
+		const renderedActionItems = itemsToRenderIds.map((itemId, index) => {
+			const actionObject = this.generateActionObject(itemId);
+			return this.generateActionItemJSX(actionObject, index, renderType);
+		});
+		return renderedActionItems;
+	};
 
-  generateActionObject = actionItemId => {
-    const actionItemInDB = this.state.actions[actionItemId];
+	generateActionObject = actionItemId => {
+		const actionItemInDB = this.state.actions[actionItemId];
 
-    const lib = actionsLibrary;
-    const actionItemInLib = { ...lib[actionItemInDB.actionType] };
+		const lib = actionsLibrary;
+		const actionItemInLib = { ...lib[actionItemInDB.actionType] };
 
-    const fullActionItem = Object.assign({}, actionItemInLib, actionItemInDB);
-    return fullActionItem;
-  };
+		const fullActionItem = Object.assign({}, actionItemInLib, actionItemInDB);
+		return fullActionItem;
+	};
 
-  generateActionItemJSX = (actionItem, index, renderType) => {
-    if (renderType === this.actionsItemsTypes.tabItem) {
-      return <ActionsTabItem actionItem={actionItem} key={actionItem.id} index={index} />;
-    }
-  };
+	generateActionItemJSX = (actionItem, index, renderType) => {
+		if (renderType === this.actionsItemsTypes.tabItem) {
+			return <ActionsTabItem actionItem={actionItem} key={actionItem.id} index={index} />;
+		}
+	};
 
-  render() {
-    const { state, applyActionsReorder, renderActionTabItems, getFirstFiveActions, props } = this;
-    return (
-      <ActionsContext.Provider value={{ ...state, applyActionsReorder, renderActionTabItems, getFirstFiveActions }}>
-        {props.children}
-      </ActionsContext.Provider>
-    );
-  }
+	render() {
+		const { state, applyActionsReorder, renderActionTabItems, getFirstFiveActions, props } = this;
+		return (
+			<ActionsContext.Provider value={{ ...state, applyActionsReorder, renderActionTabItems, getFirstFiveActions }}>
+				{props.children}
+			</ActionsContext.Provider>
+		);
+	}
 }
 export default ActionsContextProvider;
