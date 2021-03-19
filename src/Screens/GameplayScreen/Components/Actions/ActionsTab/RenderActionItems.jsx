b22b398efc837e82452b8c733642cedfb163778d
shailee <shailee.eliyahu@gmail.com>
2021-03-19 20:18:19 +0200

    refactored a bit of RenderActionItems.jsx

--- src/Screens/GameplayScreen/Components/Actions/ActionsTab/RenderActionItems.jsx
+++ src/Screens/GameplayScreen/Components/Actions/ActionsTab/RenderActionItems.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import actionsLibrary from 'ExampleData/actionsLibraryExampleData';
 import ActionsTabItem from './ActionsTabItem';
+import assembleActionDataItem from 'Services/assembleActionDataObject';
+import texts from 'texts';
 
 const actionsItemsTypes = { tabItem: 'tabItem' }; //this is for futre use so we could have more items type
 export function renderActionTabItems(numOfItemsToRender) {
@@ -11,25 +12,16 @@ export function renderActionTabItems(numOfItemsToRender) {
 function RenderActionItems(renderType, numOfItemsToRender) {
 	const orderedActionsArray = useSelector(state => state.Actions.orderedActionsArray);
 	if (!numOfItemsToRender) numOfItemsToRender = orderedActionsArray.length;
-	const itemsIdsToRender = orderedActionsArray.slice(0, numOfItemsToRender); //returns only the first numOfItemsToRender items from the state
-	const renderedActionItems = itemsIdsToRender.map((itemId, index) => {
-		//goes throw all the numOfItemsToRender first items and creats an ActionItemJSX for each one of them
-		const actionObject = generateActionObject(orderedActionsArray, index);
-		return generateActionItemJSX(actionObject, index, renderType);
+	const itemsActionsToRender = orderedActionsArray.slice(0, numOfItemsToRender); //returns only the first numOfItemsToRender items from the state
+	const renderedActionItems = itemsActionsToRender.map((itemId, index) => {
+		//goes through all the numOfItemsToRender first items and returns a corresponding component for each one of them
+		const actionDataObject = assembleActionDataItem(orderedActionsArray, index);
+
+		if (renderType === actionsItemsTypes.tabItem) {
+			return <ActionsTabItem actionItem={actionDataObject} key={actionDataObject.id} index={index} />;
+		}
+		console.log(texts.errors.invalidActionType);
+		return null;
 	});
 	return renderedActionItems;
 }
-
-const generateActionObject = (orderedActionsArray, actionIndex) => {
-	const actionItemInDB = orderedActionsArray[actionIndex];
-	const lib = actionsLibrary;
-	const actionItemInLib = { ...lib[actionItemInDB.actionType] };
-	const fullActionItem = Object.assign({}, actionItemInLib, actionItemInDB);
-	return fullActionItem;
-};
-
-const generateActionItemJSX = (actionItem, index, renderType) => {
-	if (renderType === actionsItemsTypes.tabItem) {
-		return <ActionsTabItem actionItem={actionItem} key={actionItem.id} index={index} />;
-	}
-};
