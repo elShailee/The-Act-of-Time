bedc3a77685fdbaa8e3a538194106fb9b4b66ee0
Odeyah <odeabeles@gmail.com>
2021-04-13 11:57:33 +0300

    removed all the unnedded code becouse the react change
    removed the CSS animation and the call to the hook

--- src/Screens/Gameplay/Components/Seen/SeenButton.jsx
+++ src/Screens/Gameplay/Components/Seen/SeenButton.jsx
@@ -1,13 +1,10 @@
-import { useRef } from 'react';
 import './SeenButton.css';
 import texts from 'texts';
 import { useDispatch, useSelector } from 'react-redux';
 import { unmountComponents, mountComponents } from 'Redux/Slices/MountedComponents';
-//import useButtonAnimation from 'Utils/useButtonAnimation';
 import { useSpring, animated } from 'react-spring';
 
 export default function SeenButton() {
-	const seenMoveRef = useRef();
 	const dispatch = useDispatch();
 	const isCraftingWindowMounted = useSelector(state => state.MountedComponents.CraftingWindow);
 	const isSeenWindowMounted = useSelector(state => state.MountedComponents.SeenWindow);
@@ -16,13 +13,7 @@ export default function SeenButton() {
 	const moveSeenButtonDown = useSpring({
 		translate: atLeastOneWindowOpen ? (30, 0) : (0, 0),
 	});
-	// function seenButtonShouldMoveDown() {
-	// 	seenMoveRef.current.style.animation = 'move 0.5s linear';
-	// }
-	// function seenButtonShouldMoveUp() {
-	// 	seenMoveRef.current.style.animation = 'moveBack 0.5s linear';
-	// }
-	// useButtonAnimation(seenButtonShouldMoveUp, seenButtonShouldMoveDown, atLeastOneWindowOpen);
+
 	const onSeenClick = () => {
 		if (isSeenWindowMounted) {
 			dispatch(unmountComponents(['SeenWindow']));
@@ -31,7 +22,7 @@ export default function SeenButton() {
 		}
 	};
 	return (
-		<animated.div style={moveSeenButtonDown} className='seenButton' ref={seenMoveRef} onClick={onSeenClick}>
+		<animated.div style={moveSeenButtonDown} className='seenButton' onClick={onSeenClick}>
 			{texts.gameplay.seen.buttonTitle}
 		</animated.div>
 	);
