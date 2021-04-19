e9e87d01870711b65614fd765c03c223ee6921d6
Odeyah <odeabeles@gmail.com>
2021-04-19 11:07:17 +0300

    addresed pr issues
    changed name to be more esay to the eye

--- src/Utils/useAnimations.jsx
+++ src/Utils/useAnimations.jsx
@@ -4,19 +4,19 @@ import { useSpring } from 'react-spring';
 export default function useAnimations() {
 	const isCraftingWindowMounted = useSelector(state => state.MountedComponents.CraftingWindow);
 	const isSeenWindowMounted = useSelector(state => state.MountedComponents.SeenWindow);
-	const atLeastOneWindowMounted = isCraftingWindowMounted || isSeenWindowMounted;
+	const isAtLeastOneWindowMounted = isCraftingWindowMounted || isSeenWindowMounted;
 
 	const animateCraftingButton = useSpring({
-		translateX: atLeastOneWindowMounted ? '4vh' : '0vh',
-		translateY: atLeastOneWindowMounted ? '2vh' : '0vh',
-		rotate: atLeastOneWindowMounted ? 360 : 0,
-		scale: atLeastOneWindowMounted ? 0.6 : 1,
+		translateX: isAtLeastOneWindowMounted ? '4vh' : '0vh',
+		translateY: isAtLeastOneWindowMounted ? '2vh' : '0vh',
+		rotate: isAtLeastOneWindowMounted ? 360 : 0,
+		scale: isAtLeastOneWindowMounted ? 0.6 : 1,
 	});
 	const animateCraftingButtonText = useSpring({
-		scale: atLeastOneWindowMounted ? 1.25 : 1,
+		scale: isAtLeastOneWindowMounted ? 1.25 : 1,
 	});
 	const animateSeenButton = useSpring({
-		translateY: atLeastOneWindowMounted ? '5vh' : '0vh',
+		translateY: isAtLeastOneWindowMounted ? '5vh' : '0vh',
 	});
 	return { animateCraftingButton, animateCraftingButtonText, animateSeenButton };
 }
