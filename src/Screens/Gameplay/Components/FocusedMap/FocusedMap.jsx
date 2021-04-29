56d530f79a3b74f75ba5c448a02a123d901c133e
Yagel <yageljaoui@gmail.com>
2021-04-29 14:07:13 +0300

    fixed rows and columns issue + container blocking buttons
    bug fixing

--- src/Screens/Gameplay/Components/FocusedMap/FocusedMap.jsx
+++ src/Screens/Gameplay/Components/FocusedMap/FocusedMap.jsx
@@ -1,15 +1,15 @@
 import './FocusedMap.css';
 import Tile from './Tile.jsx';
 
-const GRID_ROW_LENGTH = 14;
-const GRID_COL_LENGTH = 30;
+const GRID_ROWS = 14;
+const GRID_COLS = 30;
 
 const newNode = Tile;
 
 const grid = [];
-for (let row = 0; row < GRID_ROW_LENGTH; row++) {
+for (let row = 0; row < GRID_ROWS; row++) {
 	const currentRow = [];
-	for (let col = 0; col < GRID_COL_LENGTH; col++) {
+	for (let col = 0; col < GRID_COLS; col++) {
 		currentRow.push(newNode);
 	}
 	grid.push(currentRow);
@@ -20,7 +20,7 @@ export default function Grid() {
 		<div className='grid'>
 			{grid.map((row, rowId) => {
 				return (
-					<div key={rowId}>
+					<div key={rowId} className='row'>
 						{row.map((tile, tileId) => {
 							return <Tile key={tileId} />;
 						})}
