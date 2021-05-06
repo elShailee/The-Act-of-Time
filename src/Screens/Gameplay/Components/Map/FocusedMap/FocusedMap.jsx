05688bc99959765a7ce94ca3e69030cc7b74acc1
Yagel <yageljaoui@gmail.com>
2021-05-06 23:13:15 +0300

    first draft - hexagonal focused map
    first attempt on making a hexagonal grid system (it works but have to be more dynamic and generic.

--- src/Screens/Gameplay/Components/Map/FocusedMap/FocusedMap.jsx
+++ src/Screens/Gameplay/Components/Map/FocusedMap/FocusedMap.jsx
@@ -2,20 +2,36 @@ import './FocusedMap.css';
 import Tile from './Tile.jsx';
 
 const GRID_ROWS = 15;
-const GRID_COLS = 26;
+let colAmount = 14;
+let rowNumber = 0;
 
 const tileExampleData = { data: 'data' }; // this is a place holder , need to be removed.
 
 const tilesArray = [];
 for (let row = 0; row < GRID_ROWS; row++) {
 	const currentRow = [];
-	for (let col = 0; col < GRID_COLS; col++) {
-		currentRow.push(tileExampleData); // Here the tile's data object given by the DB will be inserted.
+
+	if (rowNumber >= 7) {
+		// Lower half of the focused map.
+		for (let col = 0; col < colAmount; col++) {
+			currentRow.push(tileExampleData);
+		}
+		colAmount -= 2;
+		rowNumber++;
+	} else if (row === rowNumber) {
+		// Upper half of the focused map.
+		for (let col = 0; col < colAmount; col++) {
+			currentRow.push(tileExampleData);
+		}
+		colAmount += 2;
+		rowNumber++;
 	}
+
 	tilesArray.push(currentRow);
 }
 
 export default function FocusedMap() {
+	console.log(tilesArray);
 	return (
 		<div id='focusedMap'>
 			{tilesArray.map((row, rowId) => {
