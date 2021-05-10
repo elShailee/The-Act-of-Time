8da22aac83c8e8931dc192a66cfbe878022cc2fe
Yagel <yageljaoui@gmail.com>
2021-05-10 14:43:58 +0300

    hide border tiles on focusedmap

--- src/Screens/Gameplay/Components/Map/FocusedMap/Tile.jsx
+++ src/Screens/Gameplay/Components/Map/FocusedMap/Tile.jsx
@@ -1,5 +1,21 @@
 import './Tile.css';
 
 export default function Tile(props) {
-	return <div className='fm-tile' tileData={props.tileData}></div>;
+	if (props.tileData === 'blocker') {
+		return <div className='fm-hidden' tileData={props.tileData}></div>;
+	} else if (props.tileData === 'upperBorderTile') {
+		return <div className='fm-hidden' tileData={props.tileData}></div>;
+	} else if (props.tileData === 'bottomBorderTile') {
+		return <div className='fm-hidden' tileData={props.tileData}></div>;
+	} else if (props.tileData === 'upperRight') {
+		return <div className='fm-hidden' tileData={props.tileData}></div>;
+	} else if (props.tileData === 'upperLeft') {
+		return <div className='fm-hidden' tileData={props.tileData}></div>;
+	} else if (props.tileData === 'lowerRight') {
+		return <div className='fm-hidden' tileData={props.tileData}></div>;
+	} else if (props.tileData === 'lowerLeft') {
+		return <div className='fm-hidden' tileData={props.tileData}></div>;
+	} else {
+		return <div className='fm-tile' tileData={props.tileData}></div>;
+	}
 }
