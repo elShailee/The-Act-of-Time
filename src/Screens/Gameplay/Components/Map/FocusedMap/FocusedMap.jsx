5bbc41321f2ccf8f4ad2e040a61fe129c1b53514
Yagel <yageljaoui@gmail.com>
2021-05-15 13:27:30 +0300

    created assembleFocusedLayout function
    moved all the utils out of FocusedMap folder to a new folder named FocusedMapUtils inside Utils folder

--- src/Screens/Gameplay/Components/Map/FocusedMap/FocusedMap.jsx
+++ src/Screens/Gameplay/Components/Map/FocusedMap/FocusedMap.jsx
@@ -1,23 +1,8 @@
 import './FocusedMap.css';
 import Tile from './Tile.jsx';
-import { insertFocusedMapPadding } from 'Utils/insertFocusedMapPadding';
-import tileExampleData from './tileExampleData';
+import assembleFocusedLayout from 'Utils/FocusedMapUtils/assembleFocusedLayout';
 
-const GRID_ROWS = 13;
-const GRID_COLS = 26;
-
-const paddedFocusedMap = insertFocusedMapPadding(tileExampleData);
-
-const tilesArray = [];
-for (let row = 0; row < GRID_ROWS; row++) {
-	const currentRow = [];
-	let colAmount = 2 * Math.abs((GRID_ROWS - 1) / 2 - row);
-	for (let col = 0; col < GRID_COLS - colAmount; col++) {
-		currentRow.push(paddedFocusedMap[row][col]);
-	}
-
-	tilesArray.push(currentRow);
-}
+const tilesArray = assembleFocusedLayout();
 
 export default function FocusedMap() {
 	return (
---
+++ src/Utils/FocusedMapUtils/assembleFocusedLayout.js
@@ -0,0 +1,22 @@
+import tileExampleData from './tileExampleData';
+import { insertFocusedMapPadding } from './insertFocusedMapPadding';
+
+const GRID_ROWS = 13;
+const GRID_COLS = 26;
+
+const paddedFocusedMap = insertFocusedMapPadding(tileExampleData);
+
+export default function assembleFocusedLayout() {
+	const tilesArray = [];
+
+	for (let row = 0; row < GRID_ROWS; row++) {
+		const currentRow = [];
+		let colAmount = 2 * Math.abs((GRID_ROWS - 1) / 2 - row);
+		for (let col = 0; col < GRID_COLS - colAmount; col++) {
+			currentRow.push(paddedFocusedMap[row][col]);
+		}
+
+		tilesArray.push(currentRow);
+	}
+	return tilesArray;
+}
