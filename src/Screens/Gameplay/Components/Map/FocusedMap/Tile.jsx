e1ba35b3b717a22eddf6ee12dbdadc95c3ee5f3f
Yagel <yageljaoui@gmail.com>
2021-05-18 14:33:40 +0300

    review fixes 2#
    -borders tileData changed from string to object with type and value.
    - a more generic tile render on TIle.jsx.

--- src/Screens/Gameplay/Components/Map/FocusedMap/Tile.jsx
+++ src/Screens/Gameplay/Components/Map/FocusedMap/Tile.jsx
@@ -2,27 +2,8 @@ import './Tile.css';
 import focusedAssets from './focusedMapAssetLib';
 
 export default function Tile({ tileData }) {
-	//blocker tiles
-	if (tileData === 'blocker-right') {
-		return <div className='blocker-right fm-tile' tiledata={tileData}></div>;
-	} else if (tileData === 'blocker-left') {
-		return <div className='blocker-left fm-tile' tiledata={tileData}></div>;
-		//hidden tiles
-	} else if (tileData === 'upperBorderTile') {
-		return <div className='hidden fm-tile' tiledata={tileData}></div>;
-	} else if (tileData === 'bottomBorderTile') {
-		return <div className='hidden fm-tile' tiledata={tileData}></div>;
-		//teleporter tiles
-	} else if (tileData === 'upperRight') {
-		return <div className='tele-upperRight fm-tile' tiledata={tileData}></div>;
-	} else if (tileData === 'upperLeft') {
-		return <div className='tele-upperLeft fm-tile' tiledata={tileData}></div>;
-	} else if (tileData === 'lowerRight') {
-		return <div className='tele-lowerRight fm-tile' tiledata={tileData}></div>;
-	} else if (tileData === 'lowerLeft') {
-		return <div className='tele-lowerLeft fm-tile' tiledata={tileData}></div>;
-	} else {
-		//map content
-		return <div className='fm-tile' style={{ backgroundImage: `url(${focusedAssets[tileData]})` }} />;
-	}
+	if (tileData.type === 'border') {
+		return <div className={'fm-tile fm-' + tileData.value} />;
+	} else return <div className='fm-tile' style={{ backgroundImage: `url(${focusedAssets[tileData]})` }} />;
 }
+// }
--- src/Utils/FocusedMapUtils/assembleFocusedLayout.js
+++ src/Utils/FocusedMapUtils/assembleFocusedLayout.js
@@ -4,8 +4,8 @@ export default function assembleFocusedLayout(exampleData) {
 	let first = new Array(exampleData[0].length);
 	let last = new Array(exampleData[0].length);
 	for (let i = 0; i < exampleData[0].length; i++) {
-		first[i] = 'upperBorderTile';
-		last[i] = 'bottomBorderTile';
+		first[i] = { type: 'border', value: 'topBorderTile' };
+		last[i] = { type: 'border', value: 'bottomBorderTile' };
 	}
 	res[0] = first;
 	res[res.length - 1] = last;
@@ -14,14 +14,32 @@ export default function assembleFocusedLayout(exampleData) {
 		let currentRow = new Array(exampleData[row].length + 2);
 
 		if (row === Math.floor(exampleData.length / 2)) {
-			currentRow[0] = 'blocker-left';
-			currentRow[currentRow.length - 1] = 'blocker-right';
+			currentRow[0] = {
+				type: 'border',
+				value: 'blocker-left',
+			};
+			currentRow[currentRow.length - 1] = {
+				type: 'border',
+				value: 'blocker-right',
+			};
 		} else if (row < exampleData.length / 2) {
-			currentRow[0] = 'upperLeft';
-			currentRow[currentRow.length - 1] = 'upperRight';
+			currentRow[0] = {
+				type: 'border',
+				value: 'upperLeft',
+			};
+			currentRow[currentRow.length - 1] = {
+				type: 'border',
+				value: 'upperRight',
+			};
 		} else {
-			currentRow[0] = 'lowerLeft';
-			currentRow[currentRow.length - 1] = 'lowerRight';
+			currentRow[0] = {
+				type: 'border',
+				value: 'lowerLeft',
+			};
+			currentRow[currentRow.length - 1] = {
+				type: 'border',
+				value: 'lowerRight',
+			};
 		}
 
 		for (let cell = 0; cell < exampleData[row].length; cell++) currentRow[cell + 1] = exampleData[row][cell];
