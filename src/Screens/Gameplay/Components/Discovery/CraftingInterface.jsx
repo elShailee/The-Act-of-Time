5b920dc39d098cdf88a1d946874b2ca14ff0d613
shailee <shailee.eliyahu@gmail.com>
2021-08-02 19:17:45 +0300

    fixed hooks issues, need to fix createGeneralDroppable

--- src/Screens/Gameplay/Components/Discovery/CraftingInterface.jsx
+++ src/Screens/Gameplay/Components/Discovery/CraftingInterface.jsx
@@ -1,30 +1,5 @@
 import React from 'react';
-import { renderDroppablesGrid } from 'Utils/dndUtils';
 
-export default function CraftingInterface({ dndState, setDndState }) {
-	const gridConfig = {
-		rows: 3,
-		cols: 3,
-		draggablesType: 'DiscoveryItems',
-		isDropDisabled: false,
-		isDragDisabled: false,
-		isValueFixed: false,
-		isInventory: false,
-	};
-	const gridConfig2 = {
-		rows: 2,
-		cols: 2,
-		draggablesType: 'DiscoveryItems',
-		isDropDisabled: true,
-		isDragDisabled: false,
-		isValueFixed: true,
-		isInventory: true,
-	};
-
-	return (
-		<div>
-			{renderDroppablesGrid(gridConfig, dndState, setDndState)}
-			{renderDroppablesGrid(gridConfig2, dndState, setDndState)}
-		</div>
-	);
+export default function CraftingInterface({ renderInput }) {
+	return <div>{renderInput()}</div>;
 }
--- src/Screens/Gameplay/Components/Discovery/DiscoveryWindow.jsx
+++ src/Screens/Gameplay/Components/Discovery/DiscoveryWindow.jsx
@@ -1,13 +1,40 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import GeneralWindow from 'GeneralComponents/GeneralWindow/GeneralWindow';
 import GeneralNavBar from 'GeneralComponents/GeneralNavBar/GeneralNavBar';
 import styles, { UpperInterface, InventoryContainer } from './styles_window';
 import CraftingInterface from './CraftingInterface';
 import { DragDropContext } from 'react-beautiful-dnd';
-import { applyItemPlacement } from 'Utils/dndUtils';
+import { applyItemPlacement, writeGridDataByConfig, renderDroppablesGrid /*deleteGridDataByConfig*/ } from 'Utils/dndUtils';
+
+const craftingInputConfig = {
+	name: 'craftingInputConfig',
+	rows: 3,
+	cols: 3,
+	draggablesType: 'DiscoveryItems',
+	isDropDisabled: false,
+	isDragDisabled: false,
+	isValueFixed: false,
+	isInventory: false,
+};
+const inventoryConfig = {
+	name: 'inventoryConfig',
+	rows: 2,
+	cols: 2,
+	draggablesType: 'DiscoveryItems',
+	isDropDisabled: true,
+	isDragDisabled: false,
+	isValueFixed: true,
+	isInventory: true,
+};
 
 export default function DiscoveryWindow({ unmountDiscoveryWindow }) {
 	const [activeInterface, setActiveInterface] = useState('Craft');
+	const [droppablesState, setDroppablesState] = useState({});
+	useEffect(() => {
+		const res1 = writeGridDataByConfig(craftingInputConfig, {});
+		const res2 = writeGridDataByConfig(inventoryConfig, {});
+		setDroppablesState({ ...res1, ...res2 });
+	}, []);
 
 	const navBarButtons = [
 		{
@@ -30,10 +57,17 @@ export default function DiscoveryWindow({ unmountDiscoveryWindow }) {
 		/>
 	);
 
-	const [dndDroppablesState, setDndDroppablesState] = useState({});
+	useEffect(() => {
+		// console.log(droppablesState);
+	}, [droppablesState]);
 
 	return (
-		<DragDropContext onDragEnd={result => applyItemPlacement(result, dndDroppablesState, setDndDroppablesState)}>
+		<DragDropContext
+			onDragEnd={result => {
+				setDroppablesState(applyItemPlacement(result, droppablesState));
+				// console.log(droppablesState);
+			}}
+		>
 			<GeneralWindow
 				unmountGeneralWindow={unmountDiscoveryWindow}
 				position='rightSided'
@@ -44,11 +78,11 @@ export default function DiscoveryWindow({ unmountDiscoveryWindow }) {
 			>
 				<UpperInterface>
 					{activeInterface === 'Craft' && (
-						<CraftingInterface dndState={dndDroppablesState} setDndState={setDndDroppablesState} />
+						<CraftingInterface renderInput={() => renderDroppablesGrid(craftingInputConfig, droppablesState)} />
 					)}
 					{activeInterface === 'Harvest' && 'Harvesting Interface'}
 				</UpperInterface>
-				<InventoryContainer />
+				<InventoryContainer>{renderDroppablesGrid(inventoryConfig, droppablesState)}</InventoryContainer>
 			</GeneralWindow>
 		</DragDropContext>
 	);
