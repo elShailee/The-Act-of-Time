7d8e7884bec0ad0bb106954382b0ad5f2be3f003
shailee <shailee.eliyahu@gmail.com>
2021-09-22 00:35:50 +0300

    Major rendering improvement on DiscoveryItem

---
+++ src/Screens/Gameplay/Components/Discovery/DiscoveryWindow/DiscoveryItemStatic.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { getDiscoveryImage, itemsLib } from 'Assets/discovery/lib';
+import { ItemImage } from './styles_window';
+
+export default function DiscoveryItemStatic({ itemIndex, isFound }) {
+	const imageObject = itemIndex <= Object.keys(itemsLib).length ? getDiscoveryImage(itemIndex) : null;
+
+	return (
+		<div>
+			{imageObject && (
+				<ItemImage src={isFound ? imageObject.image : imageObject.silhouette} title={isFound ? imageObject.name : ''} />
+			)}
+		</div>
+	);
+}
--- src/Utils/DiscoveryUtils/dndRenderUtils.jsx
+++ src/Utils/DiscoveryUtils/dndRenderUtils.jsx
@@ -1,7 +1,9 @@
-import { Droppable } from 'react-beautiful-dnd';
+import { Droppable, Draggable } from 'react-beautiful-dnd';
 import { DiscoveryItemDroppable } from 'Screens/Gameplay/Components/Discovery/DiscoveryWindow/styles_window';
-import DiscoveryItem from 'Screens/Gameplay/Components/Discovery/DiscoveryWindow/DiscoveryItem';
 import { RowContainer, TableContainer } from './styles';
+import DiscoveryItemStatic from 'Screens/Gameplay/Components/Discovery/DiscoveryWindow/DiscoveryItemStatic';
+import DiscoveryItemByState from 'Screens/Gameplay/Components/Discovery/DiscoveryWindow/DiscoveryItemByState';
+import characterInfoExampleData from 'ExampleData/characterInfoExampleData';
 
 export const renderDroppablesGrid = ({ gridConfig }) => {
 	let result = [];
@@ -20,13 +22,49 @@ export const renderDroppablesGrid = ({ gridConfig }) => {
 
 const itemDroppableGenerator = (row, col, gridConfig) => {
 	const id = `${gridConfig.name}_r${row}c${col}`;
+	const itemIndex = row * gridConfig.cols + col + 1;
+
+	const itemIndexIfFound = characterInfoExampleData.discoveredItems.find(item => item === itemIndex);
+	const isFound = itemIndexIfFound === itemIndex;
+
+	//create a custom style to avoid dropping animation from react-beautiful-dnd.
+	const nonAnimatedStyle = (style, snapshot) => {
+		if (!snapshot.isDropAnimating) {
+			return style;
+		}
+		return {
+			...style,
+			// cannot be exactly 0, due to react-beautiful-dnd's mechanics
+			transitionDuration: `0.001s`,
+		};
+	};
 
 	return (
 		<Droppable type={gridConfig.droppablesType} droppableId={id} key={id} isDropDisabled={gridConfig.isDropDisabled}>
-			{provided => (
-				<DiscoveryItemDroppable {...provided.droppableProps} ref={provided.innerRef}>
-					{provided.placeholder}
-					<DiscoveryItem id={id} isDragDisabled={gridConfig.isDragDisabled} />
+			{droppableProvided => (
+				<DiscoveryItemDroppable {...droppableProvided.droppableProps} ref={droppableProvided.innerRef}>
+					{droppableProvided.placeholder}
+
+					<Draggable
+						draggableId={id}
+						index={0}
+						isDragDisabled={(gridConfig.isInventory && !isFound) || gridConfig.isDragDisabled}
+					>
+						{(draggableProvided, snapshot) => (
+							<div
+								ref={draggableProvided.innerRef}
+								{...draggableProvided.draggableProps}
+								{...draggableProvided.dragHandleProps}
+								style={nonAnimatedStyle(draggableProvided.draggableProps.style, snapshot)}
+							>
+								{gridConfig.isInventory ? (
+									<DiscoveryItemStatic itemIndex={itemIndex} isFound={isFound} />
+								) : (
+									<DiscoveryItemByState id={id} />
+								)}
+							</div>
+						)}
+					</Draggable>
 				</DiscoveryItemDroppable>
 			)}
 		</Droppable>
